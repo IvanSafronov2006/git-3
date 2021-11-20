c186e3328bcc2bf0ebfdd68f18e3a36339189921
Сафронов Иван <93474223+IvanSafronov2006@users.noreply.github.com>
2021-11-20 20:53:17 +0300

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,43 @@
+import sys
+import sys
+from random import randint
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.qp = QPainter()
+        self.setGeometry(300, 300, 500, 500)
+        self.btn = QPushButton('Круг?', self)
+        self.btn.resize(200, 100)
+        self.btn.move(10, 10)
+        self.btn.clicked.connect(self.paint)
+        self.show()
+        self.coords = [200, 200]
+        self.do_paint = True
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.draw()
+            self.qp.end()
+            self.do_paint = False
+            
+    def draw(self):
+        self.qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+        self.qp.setPen(QColor(0, 0, 0))
+        self.qp.drawEllipse(*self.coords, randint(20, 200), randint(20, 200))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
